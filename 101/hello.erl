568a5418f96eb1dce0ef82f08917cc1054cc5037
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-03-31 13:31:20 +0300

    give instruction and clarfication

--- 101/hello.erl
+++ 101/hello.erl
@@ -1,6 +1,16 @@
 -module(hello).
+% This is a simple Erlang module that defines a function to print "Hello, God!" to the console.
+% -module(hello). % This line declares the name of the module, which is 'hello'.
 
+% This line specifies that the function 'hello' with arity 0 (no arguments) is exported and can be called from outside the module.
 -export([hello/0]).
 
+% This function, when called, will print "Hello, God!" followed by a newline to the console.
 hello() ->
     io:format("Hello,God!~n").
+
+% to run this code, you would typically compile the module in the Erlang shell and then call the function:
+% 1. Compile the module:
+%    1> c(hello).
+% 2. Call the function:
+%    2> hello:hello().
