d86412df2cabb50be9fb64bb1aadc66f9331899a
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-04-16 15:28:36 +0300

    sublist

--- 106/strings.erl
+++ 106/strings.erl
@@ -1,6 +1,6 @@
 -module(strings).
 
--export([string/0, multiline_string/0]).
+-export([string/0, multiline_string/0, to_upper/1, to_lower/1, concat/2,substring/1, substring/2, substring/3]).
 
 % simple string
 
@@ -18,3 +18,24 @@ multiline_string() ->
 """,
     io:format("~p~n", [Val]),
     "\n".
+
+
+to_upper(Str)->
+    lists:map(fun(X) when X >= $a, X =< $z -> X - 32; (X) -> X end, Str).
+
+to_lower(Str)->
+    lists:map(fun(X) when X >= $A, X =< $Z -> X + 32; (X) -> X end, Str).
+
+concat(Str1, Str2) ->
+    Str1 ++ Str2.
+
+% substring/1, substring/2, substring/3 
+% you can just use sublist/1, sublist/2, sublist/3 from lists module but we will create our own for demonstration
+substring(Str) ->
+    lists:sublist(Str). 
+substring(Str, Start) ->
+    lists:sublist(Str, Start). 
+substring(Str, Start , Length) ->
+    lists:sublist(Str, Start, Length).
+ 
+
