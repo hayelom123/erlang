64f8a812c25ecd255cb8a776172eb0d840737616
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-04-17 21:36:51 +0300

    modify

--- 112/tail_recursion.erl
+++ 112/tail_recursion.erl
@@ -15,5 +15,5 @@ len([], Acc) ->
     Acc;
 len([_ | T], Acc) ->
     len(T, Acc + 1).
-% In this implementation, the recursive call to len is the last operation performed in the function, and the intermediate result is passed as an accumulator (Acc) to avoid the need for the function to wait for the result of the recursive call before it can return a value. This allows the compiler to optimize the tail recursion and avoid stack overflow errors for large input lists
 len(List) -> len(List, 0).
+% In this implementation, the recursive call to len is the last operation performed in the function, and the intermediate result is passed as an accumulator (Acc) to avoid the need for the function to wait for the result of the recursive call before it can return a value. This allows the compiler to optimize the tail recursion and avoid stack overflow errors for large input lists
