c56faa82bb42821b48ff646069f1d2236404128f
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-04-18 22:41:04 +0300

    add sword and black_knight functions for enhanced exception handling

--- 114/exception.erl
+++ 114/exception.erl
@@ -1,5 +1,5 @@
 -module(exception).
--export([divide/2, errors/1, exits/1]).
+-export([divide/2, errors/1, exits/1, sword/1, black_knight/1]).
 
 % In Erlang, exceptions are used to handle errors and unexpected situations in a controlled manner.
 % When an exception occurs, the normal flow of the program is interrupted, and the control is
@@ -51,4 +51,20 @@ exits(F) ->
         _ -> ok
     catch
         exit:Exit -> {exit, caught, Exit}
+    end.
+
+sword(1) -> throw(slice);
+sword(2) -> erlang:error(cut_arm);
+sword(3) -> exit(cut_leg);
+sword(4) -> throw(punch);
+sword(5) -> exit(cross_bridge).
+
+black_knight(Attack) when is_function(Attack, 0) ->
+    try Attack() of
+        _ -> "None shall pass."
+    catch
+        throw:slice -> "It is but a scratch.";
+        error:cut_arm -> "I've had worse.";
+        exit:cut_leg -> "Come on you pansy!";
+        _:_ -> "Just a flesh wound."
     end.
