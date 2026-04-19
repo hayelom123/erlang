fe90d510a029db89118fec33afb65409256c3189
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-04-20 01:59:14 +0300

    message receiving

---
+++ 118/messages.erl
@@ -0,0 +1,18 @@
+-module(messages).
+-export([dolphine/0]).
+
+dolphine() ->
+    io:format("Dolphine is swimming in the ocean!~n"),
+    receive
+        do_flip ->
+            io:format("Dolphine does a flip!~n"),
+            dolphine();
+        do_jump ->
+            io:format("Dolphine jumps out of the water!~n"),
+            dolphine();
+        stop ->
+            io:format("Dolphine is stopping. Goodbye!~n");
+        _ ->
+            io:format("Dolphine doesn't understand the command. Try again.~n"),
+            dolphine()
+    end.
