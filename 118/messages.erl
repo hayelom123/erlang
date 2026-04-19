b8cac35f72fba0fe9100b43c6be13390116b9ede
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-04-20 02:23:11 +0300

    export send/2 function in messages module

--- 118/messages.erl
+++ 118/messages.erl
@@ -1,5 +1,5 @@
 -module(messages).
--export([dolphine/0]).
+-export([dolphine/0, send/2]).
 
 dolphine() ->
     io:format("Dolphine is swimming in the ocean!~n"),
@@ -11,8 +11,12 @@ dolphine() ->
             io:format("Dolphine jumps out of the water!~n"),
             dolphine();
         stop ->
-            io:format("Dolphine is stopping. Goodbye!~n");
+            io:format("Dolphine is stopping. Goodbye!~n"),
+            ok;
         _ ->
             io:format("Dolphine doesn't understand the command. Try again.~n"),
             dolphine()
     end.
+
+send(Pid, Command) ->
+    Pid ! Command.
