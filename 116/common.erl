7dc727e0f8a0cbb5669927bc0d4a6930f7971316
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-04-20 00:26:43 +0300

    using record for guarding and pattern matching

--- 116/common.erl
+++ 116/common.erl
@@ -1,6 +1,14 @@
 -module(common).
 
--export([first_robot/0, create_robot/0, main/1, send_message/3]).
+-export([
+    first_robot/0,
+    create_robot/0,
+    main/1,
+    send_message/3,
+    admin_panel/1,
+    adult_section/1,
+    driving_license/1
+]).
 % -compile(export_all).
 
 -record(robot, {
@@ -34,3 +42,23 @@ send_message(Sender, Receiver, Content) ->
     Sender1 = Message#message.sender,
     io:format("Message details using pattern matching - Sender: ~p\n", [Sender1]),
     io:format("Message details using pattern matching - Receiver: ~p\n", [Receiver1]).
+
+-record(user, {name, group, age}).
+% use pattern matching for fileter
+
+admin_panel(#user{name = Name, group = admin}) ->
+    io:format("Welcome, Admin ~p! You have full access to the system.~n", [Name]);
+admin_panel(#user{name = Name}) ->
+    io:format("~p is not an admin. Access denied.", [Name]).
+
+adult_section(U = #user{}) when U#user.age >= 18 ->
+    io:format("Welcome, ~p! You have access to the adult section.~n", [U#user.name]);
+adult_section(#user{name = Name}) ->
+    io:format("Sorry, ~p. You must be at least 18 years old to access the adult section.", [Name]).
+
+driving_license(#user{name = Name, age = Age}) when Age >= 16 ->
+    % Debugging line to check the age value default is undefined
+    io:format("Age: ~p\n", [Age]),
+    io:format("Congratulations, ~p! You are eligible for a driving license.~n", [Name]);
+driving_license(#user{name = Name}) ->
+    io:format("Sorry, ~p. You must be at least 16 years old to get a driving license.", [Name]).
