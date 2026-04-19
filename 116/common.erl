44be7b069b7f137eba863f8112b297d90bd1fe35
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-04-19 23:52:39 +0300

    Refactor code structure for improved readability and maintainability

--- 116/common.erl
+++ 116/common.erl
@@ -1,6 +1,6 @@
 -module(common).
 
--export([first_robot/0, create_robot/0, main/1]).
+-export([first_robot/0, create_robot/0, main/1, send_message/3]).
 % -compile(export_all).
 
 -record(robot, {
@@ -9,6 +9,7 @@
     hobbies,
     details = []
 }).
+-record(message, {sender, receiver, content}).
 % create() -> create("Mechtron", "R2D2", ["beeping", "whistling"]).
 first_robot() ->
     #robot{
@@ -21,3 +22,7 @@ main(Args) ->
 create_robot() ->
     Robot = first_robot(),
     io:format("Robot: ~p\n", [Robot]).
+
+send_message(Sender, Receiver, Content) ->
+    Message = #message{sender = Sender, receiver = Receiver, content = Content},
+    io:format("Message sent: ~p\n", [Message]).
