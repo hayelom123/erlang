fb997d88af38a0a9cb0f64823acf43371752c7e3
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-04-22 00:57:17 +0300

    add documentation comments to clarify process linking behavior

--- 119/links.erl
+++ 119/links.erl
@@ -18,3 +18,11 @@ chain(N) ->
     receive
         _ -> ok
     end.
+% After the process running linkmon:chain(0) dies, the error is propagated
+% down the chain of links until the shell process itself dies because of it. The
+% crash could have happened in any of the linked processes. Because links are
+% bidirectional, you need only one of them to die for the others to follow suit.
+
+% Links cannot be stacked. If you call link/1 fifteen times for the same two processes,
+% only one link will still exist between those processes, and a single call to unlink/1 will
+% be enough to tear it down.
