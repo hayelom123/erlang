0a9f9d264bb6d0e45c56355c8acf9f95f6de680d
Hayelom Kiros <hayelomkiros20@gmail.com>
2026-04-24 00:13:47 +0300

    refactor loop function in event module to handle timeouts with a list for better management

--- 120/reminder/src/event.erl
+++ 120/reminder/src/event.erl
@@ -18,12 +18,19 @@ normalize(N) ->
     Limit = 49 * 24 * 60 * 60,
     [N rem Limit | lists:duplicate(N div Limit, Limit)].
 
-loop(S = #state{server = Server}) ->
+%% Loop uses a list for times in order to go around the ~49 days limit
+%% on timeouts.
+loop(S = #state{server = Server, to_go = [T | Next]}) ->
     receive
         {Server, Ref, cancel} ->
             Server ! {Ref, ok}
-    after S#state.to_go * 1000 ->
-        Server ! {done, S#state.name}
+    after T * 1000 ->
+        if
+            Next =:= [] ->
+                Server ! {done, S#state.name};
+            Next =/= [] ->
+                loop(S#state{to_go = Next})
+        end
     end.
 %%% event's innards
 init(Server, EventName, Delay) ->
